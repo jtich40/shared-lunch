93e4f840545ee3ee111b8286dcf3e4ebe149b60b
knoxjess <110634800+knoxjess@users.noreply.github.com>
2023-03-22 17:57:17 -0500

    made Sequelize changes and password /.env

--- config/connections.js
+++ config/connections.js
@@ -1,10 +1,10 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME,
      process.env.DB_USER, 
-     process.env.DB_PASS, {
+     process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
         port : 3306,
