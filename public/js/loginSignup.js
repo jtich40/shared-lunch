bf672e69ab5d1919f8bd3ffeb53684e7170c6296
Jemilah Leonard <jemilah.leonard4@gmail.com>
2023-03-28 19:59:19 -0500

    added loginSignup.js & logout.js

---
+++ public/js/loginSignup.js
@@ -0,0 +1,39 @@
+const loginHandler = async (event) => {
+    event.preventDefault();
+    const username = document.querySelector("user-login").value.trim();
+    const password = document.querySelector("user-password").value.trim();
+    if (username && password) {
+        const response = await fetch("/api/user/login", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.ok) {
+            document.location.replace("/dashboard");
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+const signupHandler = async (event) => {
+    event.preventDefault();
+    const username = document.querySelector("username-signup").value.trim();
+    const password = document.querySelector("password-signup").value.trim();
+    if (username && password) {
+        const response = await fetch("/api/user/signup", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.ok) {
+            document.location.replace("/dashboard");
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector(".login-form").addEventListener("submit", loginHandler);
+document.querySelector(".signup-form").addEventListener("submit", signupHandler);
+
---
+++ public/js/logout.js
@@ -0,0 +1,14 @@
+const logoutHandler = async (event) => {
+    event.preventDefault();
+    const response = await fetch("/api/user/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+        document.location.replace("/");
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector(".logout-form").addEventListener("click", logoutHandler);
